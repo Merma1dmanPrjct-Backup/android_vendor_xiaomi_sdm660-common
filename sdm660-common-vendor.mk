8ed20893061e3489ccd955a84cba0417d1766015
clarencelol <clarencekuiek@icloud.com>
2022-09-23 20:22:33 +0800

    sdm660-common: Nuke msm_irqbalance
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: Merma1dmanPrjct <febricahya12345@gmail.com>

--- sdm660-common-vendor.mk
+++ sdm660-common-vendor.mk
@@ -195,8 +195,11 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sdm660-common/proprietary/vendor/bin/loc_launcher:$(TARGET_COPY_OUT_VENDOR)/bin/loc_launcher \
     vendor/xiaomi/sdm660-common/proprietary/vendor/bin/lowi-server:$(TARGET_COPY_OUT_VENDOR)/bin/lowi-server \
     vendor/xiaomi/sdm660-common/proprietary/vendor/bin/mm-pp-dpps:$(TARGET_COPY_OUT_VENDOR)/bin/mm-pp-dpps \
+<<<<<<< HEAD
     vendor/xiaomi/sdm660-common/proprietary/vendor/bin/msm_irqbalance:$(TARGET_COPY_OUT_VENDOR)/bin/msm_irqbalance \
     vendor/xiaomi/sdm660-common/proprietary/vendor/bin/mutualex:$(TARGET_COPY_OUT_VENDOR)/bin/mutualex \
+=======
+>>>>>>> 7d36dc7 (sdm660-common: Nuke msm_irqbalance)
     vendor/xiaomi/sdm660-common/proprietary/vendor/bin/netmgrd:$(TARGET_COPY_OUT_VENDOR)/bin/netmgrd \
     vendor/xiaomi/sdm660-common/proprietary/vendor/bin/pd-mapper:$(TARGET_COPY_OUT_VENDOR)/bin/pd-mapper \
     vendor/xiaomi/sdm660-common/proprietary/vendor/bin/pm-proxy:$(TARGET_COPY_OUT_VENDOR)/bin/pm-proxy \
